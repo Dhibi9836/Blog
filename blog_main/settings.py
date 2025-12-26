017958b14519d7a7fcd86a6825010583468ebb72
Dhibi Pradeep <dhibipradeep@gmail.com>
2025-12-26 14:28:15 +0530

    Some more funcationalities

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -59,6 +59,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'blogs.context_processors.get_categories',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+# from django.http import HttpResponse
+from .models import Blog, Category
 
-# Create your views here.
+def groupby_category(request, category_id) :
+    posts = Blog.objects.filter(category = category_id, status = "Published")
+    category_name = get_object_or_404(Category, pk = category_id)
+    # try :
+    #     category_name = Category.objects.get(pk = category_id)
+    # except :
+    #     return redirect("home")
+    context = {'posts' : posts, 'category' : category_name}
+    return render(request, "grouped_posts.html", context)
